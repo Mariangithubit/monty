1f1036b412a1f321b5c31660a3768bbe428ec454
Mariangithubit <metsyb1@hotmail.com>
2023-06-20 15:42:41 -0700

    push

---
+++ push.c
@@ -0,0 +1,43 @@
+#include "monty.h"
+
+/**
+ * f_push - add element to stack
+ * @head: stack head
+ * @counter: line_number
+ * Return: void
+ */
+void f_push(stack_t **head, unsigned int counter)
+{
+	int n, i = 0, flag = 0;
+
+	if (bus.arg[0] == '-')
+		i++;
+	
+	for (; bus.arg[i] != '\0'; i++)
+	{
+		if (bus.arg[i] > 57 || bus.arg[i] < 48)
+			flag = 1;
+	}
+
+	if (flag == 1)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n, counter");
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		fprintf(stderr, "L%d: usage: push integer\n, counter");
+                fclose(bus.file);
+                free(bus.content);
+                free_stack(*head);
+                exit(EXIT_FAILURE);
+	}
+	n = atio(bus.arg);
+	if (bus.lifi == 0)
+		add_node(head, n);
+	else
+		add_queue(head, n);
+}
