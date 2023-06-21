6383ab9df37920b5205f16e500b89ab30adb3dab
Mariangithubit <metsyb1@hotmail.com>
2023-06-21 11:48:08 -0700

    stack

---
+++ stack.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * f_stack - sets the format of the data to a stack (LIFO0
+ * @head: the head of stack
+ * @counter: line_number
+ * Return: void
+ */
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
