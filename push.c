b3842b14913bfeb590a1f7f02156281309bfd631
Mariangithubit <metsyb1@hotmail.com>
2023-06-21 14:44:14 -0700

    push

--- push.c
+++ push.c
@@ -10,31 +10,33 @@ void f_push(stack_t **head, unsigned int counter)
 {
 	int n, i = 0, flag = 0;
 
-	if (bus.arg[0] == '-')
-		i++;
-	for (; bus.arg[i] != '\0'; i++)
+	if (bus.arg)
 	{
-		if (bus.arg[i] > 57 || bus.arg[i] < 48)
-			flag = 1;
-	}
-
-	if (flag == 1)
-	{
-		fprintf(stderr, "L%d: usage: push integer\n, counter");
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
+		if (bus.arg[0] == '-')
+			i++;
+		for (; bus.arg[i] != '\0'; i++)
+		{
+			if (bus.arg[i] > 57 || bus.arg[i] < 48)
+				flag = 1;
+		}
+		if (flag == 1)
+		{
+			fprintf(stderr, "L%d: usage: push integer\n", counter);
+			fclose(bus.file);
+			free(bus.content);
+			free_stack(*head);
+			exit(EXIT_FAILURE);
+		}
 	}
 	else
 	{
-		fprintf(stderr, "L%d: usage: push integer\n, counter");
+		fprintf(stderr, "L%d: usage: push integer\n", counter);
 		fclose(bus.file);
 		free(bus.content);
 		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
-	n = atio(bus.arg);
+	n = atoi(bus.arg);
 	if (bus.lifi == 0)
 		add_node(head, n);
 	else
