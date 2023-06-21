e454cacd619587c05404e24eb9cc30b6d720b228
Mariangithubit <metsyb1@hotmail.com>
2023-06-21 15:05:38 -0700

    push

--- push.c
+++ push.c
@@ -38,7 +38,7 @@ void f_push(stack_t **head, unsigned int counter)
 	}
 	n = atoi(bus.arg);
 	if (bus.lifi == 0)
-		add_node(head, n);
+		addnode(head, n);
 	else
-		add_queue(head, n);
+		addqueue(head, n);
 }
