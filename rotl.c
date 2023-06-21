922ba68de30648150f9288b130bf47080903cb3e
Mariangithubit <metsyb1@hotmail.com>
2023-06-21 14:46:55 -0700

    rotl

--- rotl.c
+++ rotl.c
@@ -2,7 +2,7 @@
 
 /**
  * f_rotl - rotates the stack to the top.
- * @head: the head of stack 
+ * @head: the head of stack
  * @counter: line_number
  * Return: void
  */
@@ -12,16 +12,9 @@ void f_rotl(stack_t **head, __attribute__((unused)) unsigned int counter)
 
 	if (*head == NULL || (*head)->next == NULL)
 	{
-		return (0);
+		return;
 	}
 	h = (*head)->next;
 	h->prev = NULL;
 	while (temp->next != NULL)
-	{
-		temp = temp->next;
-	}
-	temp->next = *head;
-	(*head)->next = NULL;
-	(*head)->prev = temp;
-	(*head) = h;
 }
