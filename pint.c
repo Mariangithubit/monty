d509e7779df44716988ee239d39e1245db90682b
Mariangithubit <metsyb1@hotmail.com>
2023-06-21 14:27:54 -0700

    pint

--- pint.c
+++ pint.c
@@ -10,7 +10,7 @@ void f_pint(stack_t **head, unsigned int counter)
 {
 	if (*head == NULL)
 	{
-		fprintf(stderr, "L%d: can't pint, stack empty\n, counter");
+		fprintf(stderr, "L%d: can't pint, stack empty\n", counter);
 		fclose(bus.file);
 		free(bus.content);
 		free_stack(*head);
