632b6cfc373c2a3b5c7065b54a36dd94c428e23d
Mariangithubit <metsyb1@hotmail.com>
2023-06-22 14:13:45 -0700

    rotl

--- monty.h
+++ monty.h
@@ -74,5 +74,6 @@ void f_rotl(stack_t **head, __attribute__((unused)) unsigned int counter);
 void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter);
 void f_stack(stack_t **head, unsigned int counter);
 void f_queue(stack_t **head, unsigned int counter);
+int execute(char *content, stack_t **stack, unsigned int counter, FILE *file);
 
 #endif
--- rotl.c
+++ rotl.c
@@ -8,13 +8,20 @@
  */
 void f_rotl(stack_t **head, __attribute__((unused)) unsigned int counter)
 {
-	stack_t *temp = *head, *h;
+	stack_t *temp = *head, *x;
 
 	if (*head == NULL || (*head)->next == NULL)
 	{
 		return;
 	}
-	h = (*head)->next;
-	h->prev = NULL;
+	x = (*head)->next;
+	x->prev = NULL;
 	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+	temp->next = *head;
+	(*head)->next = NULL;
+	(*head)->prev = temp;
+	(*head) = x;
 }
