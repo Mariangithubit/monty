1d89e7e4448f5eca889814a63b204d63ef15e3e5
Mariangithubit <metsyb1@hotmail.com>
2023-06-22 14:04:54 -0700

    execute

---
+++ execute.c
@@ -0,0 +1,57 @@
+#include "monty.h"
+
+/**
+ * execute - excute the opcode
+ * @content: line content
+ * @stack: stack
+ * @counter: count the number of lines
+ * @file: monty file
+ * Return: void
+ */
+int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
+{
+	instruction_t opst[] = {
+		{"push", f_push},
+		{"pall", f_pall},
+		{"pint", f_pint},
+		{"pop", f_pop},
+		{"add", f_add},
+		{"swap", f_swap},
+		{"nop", f_nop},
+		{"sub", f_sub},
+		{"mul", f_mul},
+		{"div", f_div},
+		{"mod", f_mod},
+		{"pstr", f_pstr},
+		{"rotl", f_rotl},
+		{"rotr", f_rotr},
+		{"pchar", f_pchar},
+		{"stack", f_stack},
+		{"queue", f_queue},
+		{NULL, NULL}
+	};
+	unsigned int i = 0;
+	char *op = strtok(content, " \n\t");
+
+	if (op && op[0] == '#')
+		return (0);
+	bus.arg = strtok(NULL, " \n\t");
+	while (opst[i].opcode && op)
+	{
+		if (strcmp(op, opst[i].opcode) == 0)
+		{
+			opst[i].f(stack, counter);
+			return (0);
+		}
+		i++;
+	}
+	if (op && opst[i].opcode == NULL)
+	{
+		fprintf(stderr, "L%d: unknown instruction %s\n", counter, op);
+		fclose(file);
+		free(content);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+	return (1);
+}
